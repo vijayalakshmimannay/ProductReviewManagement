3256dd41b1272fa03205b3c6ad66c11499d7766e
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-27 13:25:55 +0530

    [vijayalakshmi] retreive top3 high rating records from list using LINQ

--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -12,12 +12,12 @@ namespace ProductReviewManagement
             //UC1 
             List<ProductReview> productReviewList = new List<ProductReview>()
             {
-                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 10, Review = "Good", isLike = true },
                 new ProductReview() { ProductID = 2, UserID = 2, Rating = 2, Review = "Bad", isLike = false },
                 new ProductReview() { ProductID = 3, UserID = 3, Rating = 4, Review = "Nice", isLike = true },
                 new ProductReview() { ProductID = 4, UserID = 4, Rating = 5, Review = "Good", isLike = true },
                 new ProductReview() { ProductID = 5, UserID = 5, Rating = 2, Review = "Bad", isLike = false },
-                new ProductReview() { ProductID = 6, UserID = 10, Rating = 4, Review = "Nice", isLike = true },
+                new ProductReview() { ProductID = 6, UserID = 10, Rating = 8, Review = "Nice", isLike = true },
                 new ProductReview() { ProductID = 7, UserID = 6, Rating = 3, Review = "Average", isLike = true },
                 new ProductReview() { ProductID = 8, UserID = 5, Rating = 2, Review = "Bad", isLike = true },
                 new ProductReview() { ProductID = 9, UserID = 10, Rating = 5, Review = "Good", isLike = true },
@@ -29,7 +29,7 @@ namespace ProductReviewManagement
                 new ProductReview() { ProductID = 15, UserID = 51, Rating = 4, Review = "Nice", isLike = true },
                 new ProductReview() { ProductID = 16, UserID = 8, Rating = 1, Review = "VeryBad", isLike = false },
                 new ProductReview() { ProductID = 17, UserID = 18, Rating = 3, Review = "Average", isLike = true },
-                new ProductReview() { ProductID = 18, UserID = 9, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 18, UserID = 9, Rating = 9, Review = "Good", isLike = true },
                 new ProductReview() { ProductID = 19, UserID = 10, Rating = 4, Review = "Nice", isLike = false },
                 new ProductReview() { ProductID = 20, UserID = 7, Rating = 3, Review = "Average", isLike = false },
                 new ProductReview() { ProductID = 21, UserID = 22, Rating = 5, Review = "Good", isLike = true },
@@ -40,7 +40,8 @@ namespace ProductReviewManagement
 
             };
             ProductReviewManagement.ReviewManagement reviewManagement = new ProductReviewManagement.ReviewManagement();
-            reviewManagement.getReview(productReviewList);
+            // reviewManagement.getReview(productReviewList);
+            reviewManagement.TopRecords(productReviewList);
 
         }
     }
